12c5df3a1fa02d6e2fad328ce2466e39597388da
Oleksii Khivrenko <alexeykhivrenko@gmail.com>
2016-07-11 22:42:27 +0300

    deleted loop1

--- GoIT/markup_fe4/js/js_module_1-2/2/js/script.js
+++ GoIT/markup_fe4/js/js_module_1-2/2/js/script.js
@@ -12,7 +12,7 @@ alert('Введенные имена ' + add.join('; '));
 
 var userName = prompt('Ввести имя пользователя:');
 
-    loop1: for (var n = 0; n < add.length; n++){
+    for (var n = 0; n < add.length; n++){
         if (userName == add[n]) {
             var comparison = true;
             alert (userName + ', вы успешно вошли');
