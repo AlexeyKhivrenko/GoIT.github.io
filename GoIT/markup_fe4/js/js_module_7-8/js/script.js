20f94bf9593f9c6631442ad73f9135ad7b4f7c38
Alexey Khivrenkogit config --globalgit configgit init <alexeykhivrenko@gmail.com>
2016-09-02 15:05:03 +0300

    reduce code

--- GoIT/markup_fe4/js/js_module_7-8/js/script.js
+++ GoIT/markup_fe4/js/js_module_7-8/js/script.js
@@ -1,57 +1,23 @@
 $(function() {
-  tab1 = $('.tab1');
-  tab2 = $('.tab2');
-  tab3 = $('.tab3');
-  boxTab1 = $('.boxTab1');
-  boxTab2 = $('.boxTab2');
-  boxTab3 = $('.boxTab3');
 
-
-  $('.tabs li').on('click', function() {
+  $('.tabs li').on('click', function(event) {
     if ($(this).hasClass('active')) {
       return false;
     }
+    if ($('.tabs li').hasClass('active')) {
+      $('.tabs li').removeClass('active');
+    }
     $(this).toggleClass('active');
+    var number = $(this).index();
+    var numberOfTab = '.boxTab' + number;
+    $('.boxTabs p').hide();
+    $(numberOfTab).show();
   });
 
-  function checkTab1() {
-      if (tab1.hasClass('active')) {
-        tab1.removeClass('active');
-        boxTab1.hide();
-      }
-  }
 
-  function checkTab2() {
-      if (tab2.hasClass('active')) {
-        tab2.removeClass('active');
-        boxTab2.hide();
-      }
-  }
 
-  function checkTab3() {
-      if (tab3.hasClass('active')) {
-        tab3.removeClass('active');
-        boxTab3.hide();
-      }
-  }
 
-  tab1.on('click', function() {
-    checkTab2();
-    checkTab3();
-    boxTab1.show();
-  });
-
-  tab2.on('click', function() {
-    checkTab1();
-    checkTab3();
-    boxTab2.show();
-  });
-
-  tab3.on('click', function() {
-    checkTab1();
-    checkTab2();
-    boxTab3.show();
-  });
+// ---------------------------------------------------------------------
 
   $("input[name='firstName']").on('mouseover', function() {
     $('.helpField1').show();
