34589a3745a09f9c4397393dc926c590b4f626e2
Alexey Khivrenkogit config --globalgit configgit init <alexeykhivrenko@gmail.com>
2016-10-05 16:21:00 +0300

    livereload, watch

--- GoIT/markup_fe4/js/js_module_17-18/example_with_gulp/gulpfile.js
+++ GoIT/markup_fe4/js/js_module_17-18/example_with_gulp/gulpfile.js
@@ -1,10 +1,31 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
 
 gulp.task('scripts', function() {
-  return gulp.src('js/src/*.js')
+  return gulp.src('app/js/*.js')
   .pipe(concat('main.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('js/dist'));
+  .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', ['scripts'], function(done) {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('serve', ['scripts'], function() { // Создаем таск browser-sync
+    browserSync.init({ // Выполняем browser Sync
+        server: {
+          baseDir: './'
+        }, // Serve files from the current directory
+        notify: false // Отключаем уведомления
+    });
+
+    gulp.watch('app/js/*.js', ['watch']);
+    gulp.watch('app/css/*.css', ['watch']);
+    gulp.watch('**.html', ['watch']);
+    // gulp.watch("*.html").on("change", reload);
 });
